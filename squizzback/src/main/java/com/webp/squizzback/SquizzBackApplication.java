d563eedc9260a133c064d7986db360dfcdeb976c
jeomxon <jeomxon@gmail.com>
2022-09-30 15:01:48 +0900

    init backend project

---
+++ squizzback/src/main/java/com/webp/squizzback/SquizzBackApplication.java
@@ -0,0 +1,13 @@
+package com.webp.squizzback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquizzBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquizzBackApplication.class, args);
+	}
+
+}
---
+++ squizzback/src/test/java/com/webp/squizzback/SquizzBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webp.squizzback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquizzBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
