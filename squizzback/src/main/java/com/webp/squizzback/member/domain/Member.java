7f2911e507639f9d2825bdec2a36ff45bc93efd3
jeomxon <jeomxon@gmail.com>
2022-09-30 15:34:32 +0900

    Add Member Entity Class

---
+++ squizzback/src/main/java/com/webp/squizzback/member/domain/Member.java
@@ -0,0 +1,20 @@
+package com.webp.squizzback.member.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+    @Id
+    @Column(name = "member_id")
+    private String id;
+
+    @Column(name = "member_pw")
+    private String password;
+
+    private String nickname;
+}
